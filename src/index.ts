ad0ef1d91b99df06cb74c11ef4c1082075b58960
ivan.dettenborn <ivan.dettenborn@cosysoft.ru>
2024-09-08 20:33:26 +0400

    debug

--- src/index.ts
+++ src/index.ts
@@ -19,8 +19,7 @@ const run = async () => {
 
         const taskIdMatch = branchName.match(/feature\/(\d+)/);
 
-        core.info(`github.context.ref: ${github.context.ref}`);
-        console.log(`github.context.ref: ${github.context.ref}`)
+        core.debug(`github.context.ref: ${github.context.ref}`);
 
         if (!taskIdMatch) {
             core.setFailed(`Не удалось извлечь идентификатор задачи из ветки: ${branchName}`);
